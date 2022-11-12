1e20355a376dbad59f63f98385b8d244f64d2792
Jordan <jclemens24@hotmail.com>
2022-11-12 12:05:19 -0500

    changed static method to instance method

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -117,15 +117,15 @@ userSchema.index({ email: 'text' });
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
 
-  this.password = await bcrypt.hash(this.password, 12);
+  this.password = await bcryptjs.hash(this.password, 12);
   this.passwordConfirm = undefined;
   next();
 });
 
 // eslint-disable-next-line prefer-arrow-callback
-userSchema.method('verifyPassword', async function (candidatePass, userPass) {
-  return await bcrypt.compare(candidatePass, userPass);
-});
+userSchema.methods.verifyPassword = async function (bodyPass, userPass) {
+  return await bcryptjs.compare(bodyPass, userPass);
+};
 
 const User = mongoose.model('User', userSchema);
 
