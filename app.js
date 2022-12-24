277639bd08500205ef8d987b2bd9c9e22ee60eaa
Jordan <jclemens24@hotmail.com>
2022-12-24 12:51:37 -0500

    moved placement of middlewares

--- app.js
+++ app.js
@@ -72,8 +72,8 @@ const io = new Server(httpServer, {
 
 // Server Port
 const port = process.env.PORT || 8000;
-app.options('*', cors(corsDelegation));
 app.use(cors(corsDelegation));
+app.options('*', cors(corsDelegation));
 app.use('/api', limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
